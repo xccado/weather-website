eb580a45b35e1559b175c0579ea881c0ed7d7f85
xccado <100438949+xccado@users.noreply.github.com>
2025-02-18 22:18:35 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const weatherRouter = require('./routes/weather');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import weatherRouter from './routes/weather.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config();
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
