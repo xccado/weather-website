d7cc65381622bda0897029320683ab03ad07f8e6
xccado <100438949+xccado@users.noreply.github.com>
2025-02-18 22:29:37 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import weatherRouter from './routes/weather.js';
@@ -10,11 +10,15 @@ const __dirname = dirname(__filename);
 dotenv.config();
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+// 视图引擎配置
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+// 中间件
+app.use(express.static(join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
+// 路由
 app.use('/', weatherRouter);
 
 const PORT = process.env.PORT || 3000;
