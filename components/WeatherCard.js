a2be2918930a47173d7102003e6fc62e1e38c01c
xccado <100438949+xccado@users.noreply.github.com>
2025-02-18 20:45:16 +0800

    Delete components directory

--- components/WeatherCard.js
+++
@@ -1,8 +0,0 @@
-export default function WeatherCard({ title, value }) {
-  return (
-    <div className="bg-gray-50 p-4 rounded">
-      <h3 className="text-gray-500 text-sm">{title}</h3>
-      <p className="text-2xl font-bold text-gray-800">{value}</p>
-    </div>
-  );
-}
